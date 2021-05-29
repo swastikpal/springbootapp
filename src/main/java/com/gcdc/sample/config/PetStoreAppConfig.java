a422d4217f0f8c94c4f3359baca2d9437d775064
Swastik Pal <swastikpal@gmail.com>
2021-05-29 22:15:45 +0530

    ldap configuration using ldif file

--- src/main/java/com/gcdc/sample/config/PetStoreAppConfig.java
+++ src/main/java/com/gcdc/sample/config/PetStoreAppConfig.java
@@ -1,22 +1,41 @@
 package com.gcdc.sample.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.gcdc.sample.interceptor.PetServiceInterceptor;
 
-@Component
-public class PetStoreAppConfig implements WebMvcConfigurer {
+@Configuration
+@EnableWebSecurity
+public class PetStoreAppConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
 	@Autowired
 	PetServiceInterceptor petServiceInterceptor;
-	
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(petServiceInterceptor);
-		//WebMvcConfigurer.super.addInterceptors(registry);
 	}
-	
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().requestMatchers(new RegexRequestMatcher("/index([\\S\\s]*?)", null))
+				.fullyAuthenticated().and().formLogin();
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.ldapAuthentication().userDnPatterns("uid={0},ou=people").groupSearchBase("ou=groups").contextSource()
+				.url("ldap://localhost:8389/dc=springframework,dc=org").and().passwordCompare()
+				.passwordEncoder(new BCryptPasswordEncoder()).passwordAttribute("userPassword");
+
+	}
 }
